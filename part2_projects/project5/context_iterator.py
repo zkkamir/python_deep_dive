aa701dfcc582c9b36443c283f9e1dfa562671e21
Amirhossein Zahmatkesh Kenari <zkkamir@gmail.com>
2021-11-10 15:06:20 +0330

    Implemented goal 2 of project 5 according to the course.

--- part2_projects/project5/context_iterator.py
+++ part2_projects/project5/context_iterator.py
@@ -1,6 +1,7 @@
 from os import path
 import csv
 from collections import namedtuple
+from contextlib import contextmanager
 from itertools import islice
 
 FILE_PATH = path.dirname(__file__)
@@ -8,37 +9,25 @@ file_names = path.join(FILE_PATH, 'cars.csv'), path.join(
     FILE_PATH, 'personal_info.csv')
 
 
-def get_dialect(file_name):
-    with open(file_name) as f:
-        return csv.Sniffer().sniff(f.read(1000))
+@contextmanager
+def parsed_data(file_name):
+    f = open(file_name, 'r')
+    try:
+        dialect = csv.Sniffer().sniff(f.read(1000))
+        # return the read head to the beginning
+        f.seek(0)
+        reader = csv.reader(f, dialect)
+        headers = map(lambda s: s.lower(), next(reader))
+        nt = namedtuple('Data', headers)
+        yield (nt(*row) for row in reader)
+    finally:
+        f.close()
 
 
-class FileParser:
-    def __init__(self, file_name):
-        self.file_name = file_name
+with parsed_data(file_names[0]) as data:
+    for row in islice(data, 5):
+        print(row)
 
-    def __enter__(self):
-        self._file = open(self.file_name, 'r')
-        self._reader = csv.reader(self._file, get_dialect(self.file_name))
-        headers = map(lambda s: s.lower(), next(self._reader))
-        self._nt = namedtuple('Data', headers)
-        return self
-
-    def __exit__(self, exc_type, exc_value, exc_tb):
-        self._file.close()
-        # If an exception occured do not silence it
-        return False
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self._file.closed:
-            raise StopIteration
-        else:
-            return self._nt(*next(self._reader))
-
-
-with FileParser(file_names[1]) as data:
-    for row in islice(data, 10):
+with parsed_data(file_names[1]) as data:
+    for row in islice(data, 5):
         print(row)
